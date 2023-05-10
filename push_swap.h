6385eb3435469ffd53ae1fdf56733bb4d10eb2c7
Iris Sanders <irissanders@live.nl>
2023-05-10 14:04:42 +0200

    updated headerfile

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: isanders <isanders@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/30 19:13:45 by isanders      #+#    #+#                 */
-/*   Updated: 2023/05/10 12:53:02 by isanders      ########   odam.nl         */
+/*   Updated: 2023/05/10 13:22:45 by isanders      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,35 +23,51 @@ typedef struct s_node {
 	struct s_node	*next;
 }	t_node;
 
-int		ft_atoi(const char *str);
+//main.c 
+int		is_correct_input(int argc, char **argv);
+
+//create_list.c
 t_node	*create_new_node(int number);
 t_node	*create_list_a(char **argv);
 int		print_list(t_node *head);
-int		is_correct_input(int argc, char **argv);
-int		input_is_number(char *argv);
-int		check_valid_input(char **argv);
+
+//error.c
 void	ft_error(char *str);
+
+//input_check.c
+int		check_valid_input(char **argv);
+int		input_is_number(char *argv);
 int		duplicate_check(int argc, char **argv);
 int		atoi_min_max(char *str);
-void	swap(t_node **head);
-void	swap_a(t_node **head_a);
-void	swap_b(t_node **head_b);
-void	swap_a_and_b(t_node **head_a, t_node **head_b);
+
+//push.c
 void	push(t_node **head_a, t_node **head_b);
 void	push_a(t_node **head_b, t_node **head_a);
 void	push_b(t_node **head_a, t_node **head_b);
-void	rotate(t_node **head);
-void	rotate_a(t_node **head_a);
-void	rotate_b(t_node **head_b);
-void	rotate_a_and_b(t_node **head_a, t_node **head_b);
+
+//reverse_rotate.c
 void	reverse_rotate(t_node **head);
 void	reverse_rotate_a(t_node **head_a);
 void	reverse_rotate_b(t_node **head_b);
 void	reverse_rotate_a_and_b(t_node **head_a, t_node **head_b);
+
+//rotate.c
+void	rotate(t_node **head);
+void	rotate_a(t_node **head_a);
+void	rotate_b(t_node **head_b);
+void	rotate_a_and_b(t_node **head_a, t_node **head_b);
+
+//swap.c
+void	swap(t_node **head);
+void	swap_a(t_node **head_a);
+void	swap_b(t_node **head_b);
+void	swap_a_and_b(t_node **head_a, t_node **head_b);
+
+//sort.c
 int		count_nodes(t_node **head);
 void	index_presort(t_node **head_a);
+void	print_index_test(t_node **head_a);
 int		a_is_not_sorted(t_node **head_a, t_node **head_b);
 void	sort(t_node **head_a, t_node **head_b);
-void	print_index_test(t_node **head_a);
 
 #endif
