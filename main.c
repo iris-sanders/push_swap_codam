db5bb3e0cbd352aed66eeb8c20f7e0d93e8b4a57
Iris Sanders <irissanders@live.nl>
2023-05-10 09:23:39 +0200

    added sort

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: isanders <isanders@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/03/30 19:07:40 by isanders      #+#    #+#                 */
-/*   Updated: 2023/05/09 16:09:45 by isanders      ########   odam.nl         */
+/*   Updated: 2023/05/09 22:27:40 by irissanders   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,16 @@ int	main(int argc, char **argv)
 		ft_error("ERROR: input not correct\n");
 	head_b = NULL;
 	head_a = create_list_a(argv);
-	print_index_test(&head_a);
-	// ft_printf("original head a\n");
-	// print_list(head_a);
-	// ft_printf("\n");
-	// ft_printf("original head b\n");
-	// print_list(head_b);
+	// print_index_test(&head_a);
+	ft_printf("original head a\n");
+	print_list(head_a);
+	ft_printf("\n");
+	ft_printf("original head b\n");
+	print_list(head_b);
+	index_presort(&head_a);
+	sort(&head_a, &head_b);
+	print_list(head_a);
+	ft_printf("\n");
+	ft_printf("original head b\n");
+	print_list(head_b);
 }
--- sort.c
+++ sort.c
@@ -6,7 +6,7 @@
 /*   By: isanders <isanders@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/07 17:13:49 by isanders      #+#    #+#                 */
-/*   Updated: 2023/05/09 16:15:42 by isanders      ########   odam.nl         */
+/*   Updated: 2023/05/09 22:22:58 by irissanders   ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,13 @@
 int	count_nodes(t_node **head)
 {
 	int		count;
-	t_node	*last;
+	t_node	*current_node;
 	
 	count = 0;
-	if (*head == NULL || (*head)->next == NULL)
-		exit(EXIT_FAILURE);
-	last = *head;
-	while (last != NULL)
+	current_node = *head;
+	while (current_node != NULL)
 	{
-		last = last->next;
+		current_node = current_node->next;
 		count++;
 	}
 	// ft_printf("%i", count);
@@ -68,5 +66,48 @@ void	print_index_test(t_node **head_a)
 	{
 		ft_printf("%i", link->index);
 		link = link->next;
+	}
+}
+
+int	a_is_not_sorted(t_node **head_a, t_node **head_b)
+{
+	t_node	*current_node_a;
+	t_node	*node_b;
+
+	node_b = *head_b;
+	if (node_b != NULL)
+		return (1);
+	current_node_a = *head_a;
+	while (current_node_a != NULL)
+	{
+		if (current_node_a->value > current_node_a->next->value)
+			return (1);
+		else
+			current_node_a = current_node_a->next;
+	}
+	return (0);
+}
+
+
+void	sort(t_node **head_a, t_node **head_b)
+{
+	int	bit;
+	int	nodes_to_check; //nodes
+
+	bit = 1;
+	while (a_is_not_sorted(head_a, head_b) == 1)
+	{
+		nodes_to_check = count_nodes(head_a);
+		while (nodes_to_check > 0)
+		{
+			if ((*head_a)->index & bit) //if it is true
+				rotate_a(head_a);
+			else						//if it is false
+				push_b(head_a, head_b);
+			nodes_to_check--;
+		}
+		bit = bit << 1;
+		while (*head_b != NULL)
+			push_a(head_b, head_a);
 	}
 }
